8deb1a3f12de98121ef63ad188da5f612d7c02b5
teejay0007 <wizadetunji@gmail.com>
2023-02-16 14:24:53 -0800

    hello-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints with printf
+ *
+ * Description: this programs prints using the printf stdio
+ *
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
