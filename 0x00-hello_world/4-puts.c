03428517ab0ae52a2885c1bd4e3df8f1c47a7e39
teejay0007 <wizadetunji@gmail.com>
2023-02-16 13:56:13 -0800

    hello-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this prints a line of code using puts
+ *
+ * Description : this program prints using the puts stdio
+ *
+ * Return:  Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
