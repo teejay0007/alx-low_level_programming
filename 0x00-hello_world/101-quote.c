7f5c47849a553ebbf20ad9c8e99f567e013bdb4b
teejay0007 <wizadetunji@gmail.com>
2023-02-17 02:56:09 -0800

    the-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - unix entry point
+ *  main - Prints out the last part of a quote in the standard error.
+ *  Return: 1 if success.
  *
- * Description: simple operating system
- *
- * Return: Always 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar,	2015-10-19\n", stdout);
-	return (1);
+		write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+		return (1);
 }
+
+
+
