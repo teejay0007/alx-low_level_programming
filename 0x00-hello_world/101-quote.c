8d7999ac09cbe669eaba031876b413bc279fa892
teejay0007 <wizadetunji@gmail.com>
2023-02-16 17:16:41 -0800

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - unix entry point
+ *
+ * Description: simple operating system
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
