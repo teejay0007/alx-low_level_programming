5eb959cc49a8441404f7690fe96b7e88bdbb604f
teejay0007 <wizadetunji@gmail.com>
2023-02-18 05:28:07 -0800

    3-print-alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - lowercase and uppercase prints
+ *
+ * Description: this program prints lower case and upper case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
