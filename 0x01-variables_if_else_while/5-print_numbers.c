d42403410785639dd907853cd4c8325932cce473
teejay0007 <wizadetunji@gmail.com>
2023-02-18 07:10:04 -0800

    5-print-numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this program prints numbers in base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	return (0);
+}
