c58933ceeac4d81ed77348f6e59467cde435e461
teejay0007 <wizadetunji@gmail.com>
2023-02-27 08:35:37 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98- this programs takses a pointer to an integer
+ *
+ * @n- pointer to an integer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this program is about pointers and arrays 
