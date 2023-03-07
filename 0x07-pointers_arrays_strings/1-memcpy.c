6594466b2855701a7a0d9b993a74d773ac05abb1
teejay0007 <wizadetunji@gmail.com>
2023-03-07 13:59:42 -0800

    task 01

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ *
+ * Return: pointer to dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int max = n;
+
+	if (max > 0)
+	{
+		int i;
+
+		for (i = 0; i < max; i++)
+		{
+			dest[i] = src[i];
+		}
+	}
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
 
 
 
